75f60a02b496de058229cc28b9f662871864aab0
Sunnykd18 <95135405+Sunnykd18@users.noreply.github.com>
2022-01-22 21:31:19 +0530

    practice

---
+++ src/Array_Backtracking_Recurssion/Practice1.java
@@ -0,0 +1,24 @@
+package Array_Backtracking_Recurssion;
+
+import java.util.Scanner;
+
+public class Practice1 { public static void main(String[]args){
+    Scanner scn = new Scanner(System.in);
+    int n = scn.nextInt();
+
+    int [] a1 = new int[n];
+    for (int i = 0; i < a1.length ; i++){
+        a1[i] = scn.nextInt();
+
+    }
+    da(a1, 0);
+}
+    static void  da(int[] a1 , int idx){
+        if (idx == a1.length){
+            return;
+        }
+        System.out.println(a1[idx]);
+        da(a1, idx + 1);
+
+    }
+}
